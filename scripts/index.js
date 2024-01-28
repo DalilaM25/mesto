3e39d04b965fb9ea1b2a05afc3557065076825b5
DalilaM25 <melissa25.dm@gmail.com>
2024-01-28 22:13:09 +0700

    Разделены функции createCard и  renderCards. deleteCard передана как аргумент.

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,27 @@
-const cardTemplate = document.querySelector("#card-template").content;
-const cardList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-const deleteCard = (evt) => evt.target.closest(".card").remove();
+const deleteCard = (evt) => evt.target.closest('.card').remove();
 
-function createCards(cardsArray) {
-    cardsArray.forEach(function (element) {
-        const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+function createCard(card, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-        cardElement.querySelector(".card__image").src = element.link;
-        cardElement.querySelector(".card__title").textContent = element.name;
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').alt = card.name;
 
-        cardList.append(cardElement);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-        const deleteButton = cardElement.querySelector(".card__delete-button");
+    deleteButton.addEventListener('click', deleteCard);
 
-        deleteButton.addEventListener("click", deleteCard);
+    return cardElement;
+}
+
+function renderCards(cardsArray) {
+    cardsArray.forEach(function(element) {
+        const newCard = createCard(element, deleteCard);
+        cardList.append(newCard);
     });
 }
 
-createCards(initialCards);
+renderCards(initialCards);
